910d526de3760475910e6e922ceef736487b2831
kyleva <kyleva@gmail.com>
2017-06-04 16:07:17 -0400

    changing pinger url

--- pinger.js
+++ pinger.js
@@ -2,7 +2,7 @@ function pinger(mins){
   const http = require('http');
   const ms = mins * 6000;
   setInterval(() => {
-    http.get('https://thelightsout-twitter-bot.herokuapp.com/')
+    http.get('//thelightsout-twitter-bot.herokuapp.com/')
     console.log('Server pinged, keep on running!');
   }, ms);
 }
