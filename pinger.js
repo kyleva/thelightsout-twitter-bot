123738cf83c21ebeef76811a87749002276066d0
kyleva <kyleva@gmail.com>
2017-06-04 16:17:43 -0400

    added missing semicolon

--- pinger.js
+++ pinger.js
@@ -2,7 +2,7 @@ function pinger(mins){
   const https = require('https');
   const ms = mins * 60000;
   setInterval(() => {
-    https.get('https://thelightsout-twitter-bot.herokuapp.com/')
+    https.get('https://thelightsout-twitter-bot.herokuapp.com/');
     console.log('Server pinged, keep on running!');
   }, ms);
 }
