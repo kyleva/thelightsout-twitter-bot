c6ee41512afa2c60caa3863e4493241bee3eec04
kyleva <kyleva@gmail.com>
2017-06-04 16:18:57 -0400

    adding console log message. checking if pinger is binding multiple times.

--- pinger.js
+++ pinger.js
@@ -1,6 +1,9 @@
 function pinger(mins){
   const https = require('https');
   const ms = mins * 60000;
+
+  console.log(`Bind pinger to run once every ${mins} minutes`);
+
   setInterval(() => {
     https.get('https://thelightsout-twitter-bot.herokuapp.com/');
     console.log('Server pinged, keep on running!');
