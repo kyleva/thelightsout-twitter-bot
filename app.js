2dd2d36ccf2cd41d0aceee8b5d5307a692ea033e
kyleva <kyleva@gmail.com>
2017-06-04 14:24:39 -0400

    small refactors in app.

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
 const Twitter = require('twitter');
 const TwitterActions = require('./twitter-actions');
 const config = require('./config');
-const app = require('express')();
-
-/**
- * Setup basic routing for root
- */
-app.get('/', (req, res) => {
-  res.send('Where are we? I do not know.');
-});
 
 /**
  * Initialize twitter instance
@@ -18,7 +10,7 @@ const T = new Twitter(config);
 /**
  * Get tweets w/ tracked phrase and do something!
  */
-T.stream('statuses/filter', {track: '#whoaitskyle'},  function(stream) {
+T.stream('statuses/filter', {track: '#tripme'},  function(stream) {
   stream.on('data', (tweet) => {
     const reply = TwitterActions.reply;
     const user = tweet.user.screen_name;
--- responses.js
+++ responses.js
@@ -1,14 +1,3 @@
-// module.exports = {
-//   get: function(){
-//     const responses = [
-//       'Who do ya think you are?',
-//       'Wuzzup fam?',
-//       'Don\'t worry -- we see you.'
-//     ];
-
-//     return responses[Math.floor(Math.random() * responses.length)];
-//   }
-// };
 const contentful = require('./contentful');
 
 function getResponse(){
