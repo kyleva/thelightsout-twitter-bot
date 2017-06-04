9585a5f8ef85a66f34b82165eb37ad2e2a32e877
kyleva <kyleva@gmail.com>
2017-06-04 14:52:34 -0400

    adding some log statements

--- app.js
+++ app.js
@@ -7,13 +7,22 @@ const config = require('./config');
  */
 const T = new Twitter(config);
 
+console.log('Initialized Twitter instance.');
+console.log(T);
+
+console.log('Look to track #tripme Tweets!');
+
 /**
  * Get tweets w/ tracked phrase and do something!
  */
 T.stream('statuses/filter', {track: '#tripme'},  function(stream) {
+  console.log('Started tracking #tripme hashtag via Streaming API.');
+
   stream.on('data', (tweet) => {
     const reply = TwitterActions.reply;
     const user = tweet.user.screen_name;
+
+    console.log('Wow, there\'s a #tripme Tweet!');
 
     const response = require('./responses').get()
     .then((response) => {
